e166f8588b10da4871c36268bc58dbd951e63ff7
Anton Bowden <bowden.anton@gmail.com>
2024-06-26 02:40:46 +0100

    improve ux

--- client.js
+++ client.js
@@ -6,20 +6,47 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
 const socket = net.createConnection(
   { host: "127.0.0.1", port: 3007 },
   async () => {
     console.log("Connected to server.");
 
-    const message = await rl.question("Enter your message > ");
+    const ask = async () => {
+      const message = await rl.question("Enter your message > ");
+      // move one line up
+      await moveCursor(0, -1);
+      // clear the line
+      await clearLine(0);
+      socket.write(message);
+    };
 
-    socket.write(message);
+    ask();
+
+    socket.on("data", async (data) => {
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+      console.log(data.toString("utf-8"));
+      ask();
+    });
   }
 );
-
-socket.on("data", (data) => {
-  console.log(data.toString("utf-8"));
-});
 
 socket.on("end", () => {
   console.log("Connection was ended.");
